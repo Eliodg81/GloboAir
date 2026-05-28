705bb5474d69438500853bc9ac7e1af656ab7b91
ELio <elio.digregorio@email.it>
2026-05-28 14:51:06 +0200

    fix: grant WebView microphone permission for Web Speech API on Android

--- android/app/src/main/java/com/globoup/globoair/MainActivity.java
+++ android/app/src/main/java/com/globoup/globoair/MainActivity.java
@@ -1,6 +1,8 @@
 package com.globoup.globoair;
 
+import android.webkit.PermissionRequest;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.BridgeWebChromeClient;
 import com.globoair.ble.BLEPeripheralPlugin;
 
 public class MainActivity extends BridgeActivity {
@@ -9,5 +11,14 @@ public class MainActivity extends BridgeActivity {
         // Registra il plugin BLEPeripheral custom
         registerPlugin(BLEPeripheralPlugin.class);
         super.onCreate(savedInstanceState);
+
+        // Permetti al WebView di usare il microfono per la Web Speech API
+        bridge.getWebView().setWebChromeClient(new BridgeWebChromeClient(bridge) {
+            @Override
+            public void onPermissionRequest(PermissionRequest request) {
+                // Concedi tutti i permessi richiesti dal WebView (microfono, camera, ecc.)
+                request.grant(request.getResources());
+            }
+        });
     }
 }
